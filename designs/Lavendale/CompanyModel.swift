79c301d1e4870a40372e3d4698f188dcbd0cf75c
Quin'darius <quinwoodz@gmail.com>
2020-03-07 19:55:30 -0500

    Adding Files

---
+++ designs/Lavendale/CompanyModel.swift
@@ -0,0 +1,15 @@
+//
+//  CompanyModel.swift
+//  designs
+//
+//  Created by Quin’darius Lyles-Woods on 3/7/20.
+//  Copyright © 2020 Quin’darius Lyles-Woods. All rights reserved.
+//
+
+import SwiftUI
+
+struct company: Identifiable {
+    var id = UUID()
+    var name: String
+    var address: String
+}
---
+++ designs/Lavendale/PackageModel.swift
@@ -0,0 +1,14 @@
+//
+//  PackageModel.swift
+//  designs
+//
+//  Created by Quin’darius Lyles-Woods on 3/7/20.
+//  Copyright © 2020 Quin’darius Lyles-Woods. All rights reserved.
+//
+import SwiftUI
+
+struct Package : Identifiable {
+    var id = UUID()
+    var company: company
+    var payout: Double
+}
