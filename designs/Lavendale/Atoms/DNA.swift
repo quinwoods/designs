a66c67df7f937c2f77ca8d4cfc0ad9667f84df4c
Quin'darius <quinwoodz@gmail.com>
2020-03-08 12:03:50 -0400

    DNA added

---
+++ designs/Lavendale/Atoms/DNA.swift
@@ -0,0 +1,16 @@
+//
+//  DNA.swift
+//  designs
+//
+//  Created by Quin’darius Lyles-Woods on 3/8/20.
+//  Copyright © 2020 Quin’darius Lyles-Woods. All rights reserved.
+//
+
+import SwiftUI
+
+let companydna = [
+    company(name: "Muse Coffee Company", address: "310 Valley Brook Ave Lyndhurst, NJ 07071"),
+    company(name: "Planted Eats", address: "61 Berdan Ave Wayne, NJ 07470"),
+    company(name: "Mod Cup", address: "479 Palisade Ave Jersey City, NJ 07307x")
+]
+
